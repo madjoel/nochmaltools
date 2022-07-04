d4873e8831b8b79ded9cdd5543a63c4a45d3ecdb
Matthieu Laqua <mlmatthieul@gmail.com>
2022-07-04 23:16:10 +0200

    Update generateboard.py to reflect changes in libnochmal.py

--- generateboard.py
+++ generateboard.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 import sys
+from random import Random
 from threading import Event
+from datetime import datetime, timedelta
 
 import libnochmal as ln
 
 
 if len(sys.argv) < 2:
-    print("Usage: {} <output_board_file> [<seed>]".format(sys.argv[0]))
+    print("Usage: {} <output_board_file> [<seed as integer>]".format(sys.argv[0]))
     sys.exit(1)
 
-seed = None
+seed = 0
 if len(sys.argv) == 3:
-    seed = sys.argv[2]
-    ln.set_seed(seed)
+    try:
+        seed = int(sys.argv[2])
+    except ValueError:
+        print("Warning: Seed is not an integer, falling back to default value.")
+RNG = Random(seed)
 
 board = ln.Board()
 
@@ -22,15 +27,30 @@ stopFlag = Event()
 thread = ln.PerpetualTimer(stopFlag, lambda: print("\rlvl. {:0>2}, try no. {}".format(state.level, state.tries), end=""), 0.1)
 thread.start()
 
+components = ln.create_random_component_order(RNG)
+
+for i in range(len(components)):
+    print("{:0>2}".format(i), end=" ")
+print()
+
+for (c, n) in components:
+    print("{}{}".format(c.value.upper(), n), end=" ")
+print()
+
+
+started = datetime.now()
+
 # actually generate the board
-ln.fill_smart(board, state)
+ln.fill_smart(board, state, components)
+
+finished = datetime.now()
 
 # this will stop the timer
 stopFlag.set()
 print("\nFinal amount of tries: {}, final level: {}".format(state.tries, state.level))
 
 # board = ln.read_board_from_file(sys.argv[1])
-ln.distribute_stars(board)
+ln.distribute_stars(board, RNG)
 
 # the result
 print(board)
