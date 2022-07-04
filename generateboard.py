60fc1d18a64c077db0932ae8eed32950b655fb90
Matthieu Laqua <mlmatthieul@gmail.com>
2022-07-04 18:54:03 +0200

    Clean up generateboard.py

--- generateboard.py
+++ generateboard.py
@@ -3,7 +3,7 @@
 import sys
 from threading import Event
 
-import libnochmal
+import libnochmal as ln
 
 
 if len(sys.argv) < 2:
@@ -13,42 +13,28 @@ if len(sys.argv) < 2:
 seed = None
 if len(sys.argv) == 3:
     seed = sys.argv[2]
-    libnochmal.set_seed(seed)
+    ln.set_seed(seed)
 
-board = libnochmal.Board()
+board = ln.Board()
 
-# randomly fill the board
-# #try:
-# #    libnochmal.fill_randomly_smarter(board)
-# #except KeyError:
-# #    print("Couldn't generate a board for seed '{}'".format(seed))
-# #    sys.exit(1)
-# #print("Successfully generated a board for seed '{}'!".format(seed))
-
-state = libnochmal.BacktrackingState()
-
+state = ln.BacktrackingState()
 stopFlag = Event()
-# thread = PerpetualTimer(stopFlag, lambda: print(board, end="\n\n"), 1.0)
-thread = libnochmal.PerpetualTimer(stopFlag, lambda: print("\rlvl. {:0>2}, try no. {}".format(state.level, state.tries), end=""), 0.1)
+thread = ln.PerpetualTimer(stopFlag, lambda: print("\rlvl. {:0>2}, try no. {}".format(state.level, state.tries), end=""), 0.1)
 thread.start()
 
-libnochmal.fill_smart(board, state)
+# actually generate the board
+ln.fill_smart(board, state)
 
 # this will stop the timer
 stopFlag.set()
 print("\nFinal amount of tries: {}, final level: {}".format(state.tries, state.level))
 
-# board = libnochmal.read_board_from_file(sys.argv[1])
-libnochmal.distribute_stars(board)
+# board = ln.read_board_from_file(sys.argv[1])
+ln.distribute_stars(board)
 
-msgs = libnochmal.check_columns(board)
-msgs.extend(libnochmal.check_stars_per_color(board))
-
-for msg in msgs:
-    print(msg)
-
+# the result
 print(board)
 
 # write the board to file
 boardFileName = sys.argv[1]
-libnochmal.write_board_to_file(board, boardFileName)
+ln.write_board_to_file(board, boardFileName)
