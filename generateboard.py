8c163b874b8db44dd579e3671fd2f707a5417a3c
Matthieu Laqua <matthieu.laqua@kit.edu>
2022-07-06 15:52:11 +0200

    Restructure generateboard.py: add signal handler for partial results

--- generateboard.py
+++ generateboard.py
@@ -4,72 +4,103 @@ import sys
 from random import Random
 from threading import Event
 from datetime import datetime
+import signal
 
 import libnochmal as ln
 
 
-if len(sys.argv) < 2:
-    print("Usage: {} <output_board_file> [<seed as integer>]".format(sys.argv[0]))
+SEED = 0
+BOARD = ln.Board()
+STATE = ln.BacktrackingState()
+FILENAME = ""
+ORDER = ['', '']
+STARTED: datetime
+
+
+def main():
+    signal.signal(signal.SIGINT, conclude_generation)
+
+    if len(sys.argv) < 2:
+        print("Usage: {} <output_board_file> [<seed as integer>]".format(sys.argv[0]))
+        sys.exit(1)
+
+    global FILENAME
+    FILENAME = sys.argv[1]
+
+    global SEED
+    if len(sys.argv) == 3:
+        try:
+            SEED = int(sys.argv[2])
+        except ValueError:
+            print("Warning: Seed is not an integer, falling back to default value.")
+    rng = Random(SEED)
+
+    stop_flag = Event()
+    thread = ln.PerpetualTimer(stop_flag, lambda: print("\rlvl. {:0>2}, placement no. {}"
+                                                        .format(STATE.level, STATE.placements), end=""), 0.1)
+    thread.start()
+
+    # components = ln.create_random_component_order(rng)
+    components = ln.create_descending_component_order()
+
+    global ORDER
+    ORDER = ['', '']
+    for i in range(len(components)):
+        print("{:0>2}".format(i), end=" ")
+        ORDER[0] += "{:0>2} ".format(i)
+    print()
+    ORDER[0] = ORDER[0].strip()
+
+    for (c, n) in components:
+        print("{}{}".format(c.value.upper(), n), end=" ")
+        ORDER[1] += "{}{} ".format(c.value.upper(), n)
+    print()
+    ORDER[1] = ORDER[1].strip()
+
+    global STARTED
+    STARTED = datetime.now()
+
+    # actually generate the board
+    ln.fill_smart(BOARD, STATE, components)
+
+    # this will stop the timer
+    stop_flag.set()
+
+    # board = ln.read_board_from_file(sys.argv[1])
+    ln.distribute_stars(BOARD, rng)
+
+    conclude_generation(-1, None)
+
+
+def conclude_generation(signum, frame):
+    finished = datetime.now()
+
+    # the result
+    print("\nFinal amount of placements: {}, final level: {}".format(STATE.placements, STATE.level))
+    print(BOARD)
+
+    aborted = False
+    if signum != -1:
+        aborted = True
+
+    # create generated board comment
+    comment = "This board was generated using nochmaltools generateboard\n" \
+              "Generation started:  {}\n" \
+              "Generation {}{}\n" \
+              "Duration:            {}\n" \
+              "Seed:                {}\n" \
+              "Component order:     {}\n" \
+              "                     {}\n" \
+              "Total placements:    {}\n" \
+              "Final level:         {}".format(STARTED, "aborted:  " if aborted else "finished: ", finished,
+                                               (finished - STARTED), SEED, ORDER[0], ORDER[1], STATE.placements,
+                                               STATE.level)
+
+    # write the board to file
+    ln.write_board_to_file(BOARD, FILENAME, comment)
+
     sys.exit(1)
 
-seed = 0
-if len(sys.argv) == 3:
-    try:
-        seed = int(sys.argv[2])
-    except ValueError:
-        print("Warning: Seed is not an integer, falling back to default value.")
-RNG = Random(seed)
 
-board = ln.Board()
-
-state = ln.BacktrackingState()
-stopFlag = Event()
-thread = ln.PerpetualTimer(stopFlag, lambda: print("\rlvl. {:0>2}, try no. {}".format(state.level, state.tries), end=""), 0.1)
-thread.start()
-
-components = ln.create_random_component_order(RNG)
-
-order_str = ['', '']
-for i in range(len(components)):
-    print("{:0>2}".format(i), end=" ")
-    order_str[0] += "{:0>2} ".format(i)
-print()
-order_str[0] = order_str[0].strip()
-
-for (c, n) in components:
-    print("{}{}".format(c.value.upper(), n), end=" ")
-    order_str[1] += "{}{} ".format(c.value.upper(), n)
-print()
-order_str[1] = order_str[1].strip()
-
-started = datetime.now()
-
-# actually generate the board
-ln.fill_smart(board, state, components)
-
-finished = datetime.now()
-
-# this will stop the timer
-stopFlag.set()
-print("\nFinal amount of tries: {}, final level: {}".format(state.tries, state.level))
-
-# board = ln.read_board_from_file(sys.argv[1])
-ln.distribute_stars(board, RNG)
-
-# the result
-print(board)
-
-# create generated board comment
-comment = "This board was generated using nochmaltools generateboard\n" \
-          "Generation started:  {}\n" \
-          "Generation finished: {}\n" \
-          "Duration:            {}\n" \
-          "Seed:                {}\n" \
-          "Component order:     {}\n" \
-          "                     {}\n" \
-          "Total tries:         {}".format(started, finished, (finished - started), seed, order_str[0], order_str[1],
-                                           state.tries)
-
-# write the board to file
-boardFileName = sys.argv[1]
-ln.write_board_to_file(board, boardFileName, comment)
+if __name__ == "__main__":
+    main()
